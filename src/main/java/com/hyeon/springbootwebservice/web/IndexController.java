e13811a3a21ef5748dd576baf036bb568c23f2be
JHB <jhyeonbin32@gmail.com>
2020-08-25 15:02:24 +0900

    google 로그인

--- src/main/java/com/hyeon/springbootwebservice/web/IndexController.java
+++ src/main/java/com/hyeon/springbootwebservice/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.hyeon.springbootwebservice.web;
 
+import com.hyeon.springbootwebservice.config.auth.LoginUser;
+import com.hyeon.springbootwebservice.config.auth.dto.SessionUser;
 import com.hyeon.springbootwebservice.service.posts.PostsService;
 import com.hyeon.springbootwebservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +19,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user!=null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
